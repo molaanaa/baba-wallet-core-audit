b0af5920c6187637ffd992959558a85b9793680c
Rayan Afzar Molaanaa <34990667+molaanaa@users.noreply.github.com>
2026-01-05 15:51:24 +0330

    Update SecureStorage.kt

--- SecureStorage.kt
+++ SecureStorage.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 BABA Wallet / Rayan Afzar Molaanaa
+ * Copyright (c) 2026 BABA Wallet / Rayan Afzar Molaanaa
  * All Rights Reserved.
  *
  * This code is part of the BABA Wallet project.
